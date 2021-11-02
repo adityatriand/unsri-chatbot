06e8930d704ff522ce1778960a4f6f797d9967ed
adityatriand <adityatriananda0@gmail.com>
2021-11-02 19:42:05 +0700

    try entities and custom actions

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,23 @@
-# This files contains
-# # This is a simple example for a custom action which utters "Hello World!"
-#
-# # from typing import Any, Text, Dict, List
-# #
-# # from rasa_sdk import Action, Tracker
-# # from rasa_sdk.executor import CollectingDispatcher
-# #
-# #
-# # class ActionHelloWorld(Action):
-# #
-# #     def name(self) -> Text:
-# #         return "action_hello_world"
-# #
-# #     def run(self, dispatcher: CollectingDispatcher,
-# #             tracker: Tracker,
-# #             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-# #
-# #         dispatcher.utter_message(text="Hello World!")
-# #
-# #         return []your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
+
+class JumlahJurusanFakultas(Action):
+
+    def name(self) -> Text:
+        return "action_jumlah_jurusan_fakultas"
+
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+    	for x in tracker.latest_message['entities']:
+    		if(x['entity']=='fakultas'):
+    			name = x['value']
+    			if(name == 'fakultas ekonomi' || name == 'FE'):
+    				dispatcher.utter_message(text="{name} masuk")
+
+        return []
 
